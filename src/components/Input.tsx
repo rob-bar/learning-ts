b075a88d354c166cbf53d61084efc11d173e1e01
Robbie Bardijn <robbie.bardijn@gmail.com>
2020-02-09 12:44:39 +0100

    lesson 10: typing useRef

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,9 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
+
+// Lesson 9
+// export const Input = () => {
+//   // TS wil infer yuor useState types via its initial value set
+//   const [name, setName] = useState('') // Type inference
+//   // const [name, setName] = useState<string>('') // type: string
+//   // const [name, setName] = useState<string | null>(null) // union type: string or null
+//   return <input value={name} onChange={e => setName(e.target.value)} />
+// }
 
 export const Input = () => {
-  // TS wil infer yuor useState types via its initial value set
-  const [name, setName] = useState('') // Type inference
-  // const [name, setName] = useState<string>('') // type: string
-  // const [name, setName] = useState<string | null>(null) // union type: string or null
-  return <input value={name} onChange={e => setName(e.target.value)} />
+  const [name, setName] = useState('')
+  // const ref = useRef<HTMLInputElement | null>(null) // this value is also writable
+
+  // this ref is read only
+  const ref = useRef<HTMLInputElement>(null!)
+
+  ref.current &&
+    ref.current.value &&
+    console.log('TCL: Input -> ref', ref.current.value)
+  return (
+    <input ref={ref} value={name} onChange={e => setName(e.target.value)} />
+  )
 }
